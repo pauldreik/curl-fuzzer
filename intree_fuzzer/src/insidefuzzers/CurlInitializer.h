f9d2bc3d27a5683f681a1674e016c13ccf66b3de
Paul Dreik <github@pauldreik.se>
2019-10-02 15:37:07 +0200

    cmake file for building from within curl

---
+++ intree_fuzzer/src/insidefuzzers/CurlInitializer.h
@@ -0,0 +1,25 @@
+#ifndef CURLINITIALIZER_H_INCLUDED
+#define CURLINITIALIZER_H_INCLUDED
+
+#include <cassert>
+#include <curl/curl.h>
+class CurlInitializer
+{
+public:
+  CurlInitializer()
+  {
+    auto ret = curl_global_init(CURL_GLOBAL_DEFAULT);
+    assert(ret == 0);
+    m_handle = curl_easy_init();
+  }
+  CURL* handle() const { return m_handle; }
+  ~CurlInitializer()
+  {
+    curl_easy_cleanup(m_handle);
+    curl_global_cleanup();
+  }
+
+private:
+  CURL* m_handle{};
+};
+#endif
